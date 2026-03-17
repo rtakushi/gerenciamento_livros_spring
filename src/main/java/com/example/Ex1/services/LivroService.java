15615d0cd5c784b24227a56c87371b254dce8741
unknown <takushirenan@gmail.com>
2026-03-17 20:26:33 -0300

    service criado

---
+++ src/main/java/com/example/Ex1/services/LivroService.java
@@ -0,0 +1,32 @@
+package com.example.Ex1.services;
+
+import com.example.Ex1.entity.Livros;
+import com.example.Ex1.repositories.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class LivroService {
+    @Autowired
+    private LivroRepository repository;
+
+    public Livros create(Livros livro){
+        return repository.save(livro);
+    }
+
+    public List<Livros> listar(){
+        return repository.findAll();
+    }
+
+    public Optional<Livros> buscarPorID(Long id){
+        return repository.findById(id);
+    }
+
+    public void delete(Long id){
+        repository.deleteById(id);
+    }
+
+}
